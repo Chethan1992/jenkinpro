fa259e9d68979eb8ea7becf5e2f262e4400ff9c3
chethan1992 <chethan1992@gmail.com>
2017-08-05 13:45:52 +0530

    jenkin example modified

--- shirt.java
+++ shirt.java
@@ -1,18 +1,7 @@
-public class shirt{
+public class shirt
+{
 public static void main(String args[])
 {
-String color="red";
-String shirt="shirt";
-switch(color){
-case "Blue": 			
-shirt="Blue" + shirt;
-//break;
-case "red": 
-shirt="red" + shirt;
-//break;
-default:
-shirt="white" + shirt;
-}
-System.out.println("shirtt type:" + shirt);
+System.out.println("hello world");
 }
 }
